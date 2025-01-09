89a1c0db3ebf6acac7acf4fadd3e801e5c53cb41
MinSung Jung <minpro22@naver.com>
2025-01-09 17:21:08 +0900

    25.01.09 fourth commit

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,31 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Slf4j
+@RestController
+public class LogTestController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+
+//        log.trace("trace log=" + name); +는 자바의 특징 때문에 () 안의 연산을 먼저 하기 때문에 절대 쓰지 말기 -> resource 낭비
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info(" info log={}", name);
+        log.warn(" warn log={}", name);
+        log.error("error log={}", name);
+
+        return "ok";
+    }
+}
