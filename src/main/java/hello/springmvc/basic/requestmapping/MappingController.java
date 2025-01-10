52605f679117aa36987720882c4ff44d087b1f3e
MinSung Jung <minpro22@naver.com>
2025-01-10 17:23:12 +0900

    25.01.10 second commit

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -2,6 +2,7 @@ package hello.springmvc.basic.requestmapping;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -60,7 +61,7 @@ public class MappingController {
 
     /**
      * 거의 잘 사용 X
-     * 파라미터로 추가 매핑
+     * 파라미터로 추가 매핑, 조건문 느낌
      * params="mode",
      * params="!mode",
      * params="mode=debug"
@@ -74,7 +75,7 @@ public class MappingController {
     }
 
     /**
-     * 특정 헤더로 추가 매핑
+     * 특정 헤더로 추가 매핑, 위와 동일 header 에 추가하는 것 차이
      * headers="mode",
      * headers="!mode",
      * headers="mode=debug",
@@ -83,6 +84,33 @@ public class MappingController {
     @GetMapping(value = "/mapping-header", headers = "mode=debug")
     public String mappingHeader() {
         log.info("mappingHeader");
+        return "ok";
+    }
+
+    /**
+     * Content-Type 헤더 기반 추가 매핑 Media Type
+     * consumes="application/json"
+     * consumes="!application/json"
+     * consumes="application/*"
+     * consumes="*\/*"
+     * MediaType.APPLICATION_JSON_VALUE
+     */
+    @PostMapping(value = "/mapping-consume", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String mappingConsumes() {
+        log.info("mappingConsumes");
+        return "ok";
+    }
+
+    /**
+     * Accept 헤더 기반 Media Type
+     * produces = "text/html"
+     * produces = "!text/html"
+     * produces = "text/*"
+     * produces = "*\/*"
+     */
+    @PostMapping(value = "/mapping-produce", produces = MediaType.TEXT_HTML_VALUE)
+    public String mappingProduces() {
+        log.info("mappingProduces");
         return "ok";
     }
 }
